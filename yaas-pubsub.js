ca01fbb9276db1014c4b9619f565a7d43ec80858
Andreas Brain | hybris <andreas.brain@hybris.de>
2015-09-29 16:34:01 +0200

    Refactoring

---
+++ yaas-pubsub.js
@@ -0,0 +1,70 @@
+var pathPubSubBase = '/hybris/pubsub/b2/topics';
+
+var requestHelper;
+
+function init(rh) {
+	requestHelper = rh;
+}
+
+function commit(topicOwnerClient, eventType, token) {
+	return requestHelper.post(
+		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/commit',
+		'application/json',
+		{
+			token: token
+		}
+	).then(function (response) {
+		if (response.statusCode == 200) {
+			return Promise.resolve();
+		} else {
+			var errorMessage = "Problem: " + JSON.stringify(response.body);
+			return Promise.reject(new Error(errorMessage));
+		}
+	});
+}
+
+/**
+ * Convert PubSub event payloads into proper nested JSON
+ */
+function fixEventPayload(events) {
+	return new Promise(function (resolve, reject) {
+		events.forEach(function(event) {
+			try {
+				event.payload = JSON.parse(event.payload);
+			} catch (e) {
+				console.log("Could not parse payload");
+				return reject(new Error("Could not parse payload: " + e.message));
+			}
+		});
+		resolve();
+	});
+}
+
+function read(topicOwnerClient, eventType, numEvents) {
+	return requestHelper.post(
+		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/read',
+		'application/json',
+		{
+			numEvents: numEvents,
+			ttlMs: 4000,
+			autoCommit: false
+		}
+	).then(function (response) {
+		if (response.statusCode == 204) {
+			return Promise.resolve();
+		} else if (response.statusCode == 200) {
+			return fixEventPayload(response.body.events).then(function() {
+				return Promise.resolve(response.body);
+			});
+		} else {
+			console.log("Problem: " + JSON.stringify(response.body));
+			return Promise.reject(new Error("Problem with request: " + JSON.stringify(response.body)));
+		}
+	});
+}
+
+module.exports = {
+	commit: commit,
+	init: init,
+	read: read
+}
--- yaas.js
+++ yaas.js
@@ -1,4 +1,5 @@
 var requestHelper = require('./yaas-requesthelper.js');
+var pubsub = require('./yaas-pubsub.js');
 
 var pathCartBase;
 var pathCheckout;
@@ -6,18 +7,19 @@ var pathCustomerBase;
 var pathOrderBase = '/hybris/order/b1';
 var pathPriceBase;
 var pathProductBase;
-var pathPubSubBase = '/hybris/pubsub/b2/topics';
 var pathSalesorderBase;
 var pathSiteBase;
 
 var debug = false;
 var verbose = false;
 
-exports.init = function (theClientId, theClientSecret, theScope) {
-	return requestHelper.begin(theClientId, theClientSecret, theScope);
+function init(theClientId, theClientSecret, theScope) {
+	return requestHelper.begin(theClientId, theClientSecret, theScope).then(function() {
+		pubsub.init(requestHelper);
+	});
 };
 
-exports.setProjectId = function (value) {
+function setProjectId(value) {
 	projectId = value;
 
 	pathCartBase = '/hybris/cart/b1/' + projectId + '/carts';
@@ -29,85 +31,19 @@ exports.setProjectId = function (value) {
 	pathSiteBase = '/hybris/site/b1/' + projectId + '/sites';
 }
 
-exports.setDebug = function (state) {
+function setDebug(state) {
 	debug = state;
 }
 
-exports.setVerbose = function (state) {
+function setVerbose(state) {
 	verbose = state;
 }
 
-/**
- * Convert PubSub event payloads into proper nested JSON
- */
-function fixEventPayload(events) {
-	return new Promise(function (resolve, reject) {
-		events.forEach(function(event) {
-			try {
-				event.payload = JSON.parse(event.payload);
-			} catch (e) {
-				console.log("Could not parse payload");
-				return reject(new Error("Could not parse payload: " + e.message));
-			}
-		});
-		resolve();
-	});
+function notYetImplemented() {
+	return Promise.reject(new Error("Method not yet implemented!"));
 }
 
-exports.commitEvents = function (topicOwnerClient, eventType, token) {
-	if (verbose) {console.log("Committing events...");}
-	
-	return requestHelper.post(
-		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/commit',
-		'application/json',
-		{
-			token: token
-		}
-	).then(function (response) {
-		if (response.statusCode == 200) {
-			if (verbose) {
-				console.log("Event(s) committed");
-			}
-			return Promise.resolve();
-		} else {
-			var errorMessage = "Problem: " + JSON.stringify(response.body);
-			if (debug) {
-				console.log(errorMessage);
-			}
-			return Promise.reject(new Error(errorMessage));
-		}
-	});
-}
-
-exports.readPubSub = function (topicOwnerClient, eventType, numEvents) {
-	return requestHelper.post(
-		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/read',
-		'application/json',
-		{
-			numEvents: numEvents,
-			ttlMs: 4000,
-			autoCommit: false
-		}
-	).then(function (response) {
-		if (response.statusCode == 204) {
-			if (verbose) {
-				console.log("No events available");
-			}
-			return Promise.resolve();
-		} else if (response.statusCode == 200) {
-			return fixEventPayload(response.body.events).then(function() {
-				return Promise.resolve(response.body);
-			});
-		} else {
-			if (debug) {
-				console.log("Problem: " + JSON.stringify(response.body));
-			}
-			return Promise.reject(new Error("Problem with request: " + JSON.stringify(response.body)));
-		}
-	});
-}
-
-exports.getSalesorderDetails = function (orderId) {
+function getSalesorderDetails(orderId) {
 	if (verbose) {console.log("Getting salesorder details for order %s...", orderId);}
 	return requestHelper.get(pathOrderBase + '/salesorders/' + orderId, {}).then(function (response) {
 		if (response.statusCode == 200) {
@@ -121,3 +57,13 @@ exports.getSalesorderDetails = function (orderId) {
 		}
 	});
 }
+
+module.exports = {
+	commitEvents: pubsub.commit,
+	getSalesorderDetails: getSalesorderDetails,
+	init: init,
+	readPubSub: pubsub.read,
+	setDebug: setDebug,
+	setProjectId: setProjectId,
+	setVerbose: setVerbose
+};
