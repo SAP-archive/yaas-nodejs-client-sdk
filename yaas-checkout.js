9ef69ae89af2b950fd902976726c1161b5ac7b1c
Andreas Brain | hybris <andreas.brain@hybris.de>
2015-10-19 15:29:28 +0200

    get checkout

--- yaas-checkout.js
+++ yaas-checkout.js
@@ -3,14 +3,19 @@ var pathCheckout;
 
 function init(rh, projectId) {
 	requestHelper = rh;
-	pathCheckout = '/hybris/checkout/b1/' + projectId + '/checkouts/order';
+	pathCheckout = '/hybris/checkout/b1/' + projectId + '/checkouts';
 }
 
 function checkout(data) {
-	return requestHelper.post(pathCheckout, 'application/json', data);
+	return requestHelper.post(pathCheckout + '/order', 'application/json', data);
+}
+
+function get(checkoutId) {
+	return requestHelper.get(pathCheckout + '/' + checkoutId, {});
 }
 
 module.exports = {
 	checkout: checkout,
+	get: get,
 	init: init
 };
