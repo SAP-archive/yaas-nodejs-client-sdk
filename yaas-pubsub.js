c9615a1d5ea9925956ae072872214c7efda7c018
Christian Neeb <c.neeb@sap.com>
2015-12-22 10:33:40 +0100

    adding createTopic and publish to yaas.js/pubsub

--- yaas-pubsub.js
+++ yaas-pubsub.js
@@ -40,13 +40,13 @@ function fixEventPayload(events) {
 	});
 }
 
-function read(topicOwnerClient, eventType, numEvents, autoCommit) {
+function read(topicOwnerClient, eventType, numEvents, autoCommit, ttlMs) {
 	return requestHelper.post(
 		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/read',
 		'application/json',
 		{
 			numEvents: numEvents,
-			ttlMs: 4000,
+			ttlMs: ttlMs || 4000,
 			autoCommit: autoCommit || false
 		}
 	).then(function (response) {
@@ -63,8 +63,44 @@ function read(topicOwnerClient, eventType, numEvents, autoCommit) {
 	});
 }
 
+function publish(topicOwnerClient, eventType, payload) {
+	return requestHelper.post(
+		pathPubSubBase + '/' + topicOwnerClient + '/' + eventType + '/publish',
+		'application/json',
+		{
+			payload: JSON.stringify(payload)
+		}
+	).then(function (response) {
+		if (response.statusCode == 201) {
+		  return Promise.resolve();
+		} else {
+			console.log("Problem: " + JSON.stringify(response.body));
+			return Promise.reject(new Error("Problem with request: " + JSON.stringify(response.body)));
+		}
+	});
+}
+
+function createTopic(eventType) {
+	return requestHelper.post(
+		pathPubSubBase,
+		'application/json',
+		{
+			eventType: eventType
+		}
+	).then(function (response) {
+		if (response.statusCode == 201) {
+		  return Promise.resolve();
+		} else {
+			console.log("Problem: " + JSON.stringify(response.body));
+			return Promise.reject(new Error("Problem with request: " + JSON.stringify(response.body)));
+		}
+	});
+}
+
 module.exports = {
 	commit: commit,
 	init: init,
-	read: read
+	read: read,
+  publish : publish,
+  createTopic : createTopic
 }
