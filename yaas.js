d2096edeb7d2aab218e3ca41ec33c6507a71c2db
Andreas Brain | hybris <andreas.brain@hybris.de>
2016-02-04 16:14:07 +0100

    Indent with 4 spaces

--- yaas.js
+++ yaas.js
@@ -10,20 +10,20 @@ var site = require('./yaas-site.js');
 var requestHelper;
 
 function init(theClientId, theClientSecret, theScope, theProjectId) {
-  requestHelper = new RequestHelper(theClientId, theClientSecret, theScope, theProjectId);
-	cart.init(requestHelper);
-	checkout.init(requestHelper);
-	customer.init(requestHelper);
-	order.init(requestHelper);
-	price.init(requestHelper);
-	product.init(requestHelper);
-	pubsub.init(requestHelper);
-	site.init(requestHelper);
-	return Promise.resolve();
+    requestHelper = new RequestHelper(theClientId, theClientSecret, theScope, theProjectId);
+    cart.init(requestHelper);
+    checkout.init(requestHelper);
+    customer.init(requestHelper);
+    order.init(requestHelper);
+    price.init(requestHelper);
+    product.init(requestHelper);
+    pubsub.init(requestHelper);
+    site.init(requestHelper);
+    return Promise.resolve();
 }
 
 function setDebug(state) {
-	requestHelper.setDebug(state);
+    requestHelper.setDebug(state);
 }
 
 function setVerbose(state) {
@@ -31,19 +31,19 @@ function setVerbose(state) {
 }
 
 function notYetImplemented() {
-	return Promise.reject(new Error("Method not yet implemented!"));
+    return Promise.reject(new Error("Method not yet implemented!"));
 }
 
 module.exports = {
-	cart: cart,
-	checkout: checkout,
-	customer: customer,
-	init: init,
-	order: order,
-	price: price,
-	product: product,
-	pubsub: pubsub,
-	setDebug: setDebug,
-	setVerbose: setVerbose,
-	site: site
+    cart: cart,
+    checkout: checkout,
+    customer: customer,
+    init: init,
+    order: order,
+    price: price,
+    product: product,
+    pubsub: pubsub,
+    setDebug: setDebug,
+    setVerbose: setVerbose,
+    site: site
 };
