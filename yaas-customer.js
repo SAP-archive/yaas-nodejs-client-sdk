95ea2ddb5c057902bc8359b29f0436595cb78fa1
Maximilian Schrupp <maximilian.schrupp@hybris.com>
2016-05-13 12:43:27 +0200

    reworked yaas-customer.js, now supporting signup, proper mime type etc.

--- yaas-customer.js
+++ yaas-customer.js
@@ -1,27 +1,28 @@
 var pathCustomerBase = '/hybris/customer/v1/{{projectId}}/';
 
 var Customer = function(rh) {
-	this.requestHelper = rh;
+    
+    this.requestHelper = rh;
 
-	this.getCustomer = function(customerNumber) {
-		return this.requestHelper.get(pathCustomerBase + 'customers/' + customerNumber, {});
-	};
+    this.getCustomer = function(customerNumber) {
+        return this.requestHelper.get(pathCustomerBase + 'customers/' + customerNumber, {});
+    };
 
-	this.getCustomers = function(queryParameters) {
-		return this.requestHelper.get(pathCustomerBase + 'customers', queryParameters);
-	};
+    this.getCustomers = function(queryParameters) {
+        return this.requestHelper.get(pathCustomerBase + 'customers', queryParameters);
+    };
 
-  this.signup = function(credentials) {
-    return this.requestHelper.post(pathCustomerBase + 'signup', "application/json", credentials);
-  };
+    this.signup = function(credentials) {
+        return this.requestHelper.post(pathCustomerBase + 'signup', "application/json", credentials);
+    };
 
-  this.updateCustomer = function(customerNumber, customer) {
+    this.updateCustomer = function(customerNumber, customer) {
     return this.requestHelper.put(pathCustomerBase + 'customers/' + customerNumber, "application/json", customer);
-  };
+    };
 
-  this.createCustomerAddress = function(customerNumber, address) {
-		return this.requestHelper.post(pathCustomerBase + 'customers/' + customerNumber + "/addresses", "application/json", address);
-  };
+    this.createCustomerAddress = function(customerNumber, address) {
+        return this.requestHelper.post(pathCustomerBase + 'customers/' + customerNumber + "/addresses", "application/json", address);
+    };
 
 };
 
