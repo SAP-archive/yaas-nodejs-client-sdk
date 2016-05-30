4c46a0d0f56b6baee18b25b37ef842d91e13414d
Georg Kreimer <georg.kreimer@hybris.com>
2016-05-30 14:21:45 +0200

    added create, get and getAll. document.write DEPRECATED! Use document.create instead.

--- yaas-document.js
+++ yaas-document.js
@@ -3,15 +3,26 @@ var pathDocumentBase = '/hybris/document/v1/{{projectId}}';
 var Document = function(rh) {
   this.requestHelper = rh;
 
+  // DEPRECATED
   this.write = function(applicationId, documentType, payload) {
-  return this.requestHelper.post(
-     pathDocumentBase + '/' + applicationId + '/data/' + documentType,
-    'application/json',
-    {
-      payload: payload
-    }
-  );
+    return this.requestHelper.post(
+              pathDocumentBase + '/' + applicationId + '/data/' + documentType,
+              'application/json',
+              { payload: payload }
+            );
   }
+
+  this.create = function(applicationId, documentType, payload) {
+    return this.requestHelper.post(pathDocumentBase + '/' + applicationId + '/data/' + documentType, 'application/json', payload);
+  };
+
+  this.getAll = function(applicationId, documentType, queryParameters) {
+    return this.requestHelper.get(pathDocumentBase + '/' + applicationId + '/data/' + documentType, queryParameters);
+  };
+
+  this.get = function(applicationId, documentType, documentId, queryParameters) {
+    return this.requestHelper.get(pathDocumentBase + '/' + applicationId + '/data/' + documentType + '/' + documentId, queryParameters);
+  };
 
 };
 
