fd15e834baa59334d406f7b6b864a92a7e5b6eef
Georg Kreimer <georg.kreimer@hybris.com>
2016-06-07 15:54:50 +0200

    added update and delete to document

--- yaas-document.js
+++ yaas-document.js
@@ -3,19 +3,6 @@ var pathDocumentBase = '/hybris/document/v1/{{projectId}}';
 var Document = function(rh) {
   this.requestHelper = rh;
 
-  // DEPRECATED
-  this.write = function(applicationId, documentType, payload) {
-    return this.requestHelper.post(
-              pathDocumentBase + '/' + applicationId + '/data/' + documentType,
-              'application/json',
-              { payload: payload }
-            );
-  }
-
-  this.create = function(applicationId, documentType, payload) {
-    return this.requestHelper.post(pathDocumentBase + '/' + applicationId + '/data/' + documentType, 'application/json', payload);
-  };
-
   this.getAll = function(applicationId, documentType, queryParameters) {
     return this.requestHelper.get(pathDocumentBase + '/' + applicationId + '/data/' + documentType, queryParameters);
   };
@@ -23,6 +10,35 @@ var Document = function(rh) {
   this.get = function(applicationId, documentType, documentId, queryParameters) {
     return this.requestHelper.get(pathDocumentBase + '/' + applicationId + '/data/' + documentType + '/' + documentId, queryParameters);
   };
+
+  this.create = function(applicationId, documentType, payload) {
+    return this.requestHelper.post(
+      pathDocumentBase + '/' + applicationId + '/data/' + documentType,
+      'application/json',
+      payload
+    );
+  };
+
+  this.update = function(applicationId, documentType, documentId, payload) {
+    return this.requestHelper.put(
+      pathDocumentBase + '/' + applicationId + '/data/' + documentType + '/' + documentId,
+      'application/json',
+      payload
+    );
+  };
+
+  this.destroy = function(applicationId, documentType, documentId) {
+    return this.requestHelper.delete(pathDocumentBase + '/' + applicationId + '/data/' + documentType + '/' + documentId);
+  };
+
+  // DEPRECATED!!!
+  this.write = function(applicationId, documentType, payload) {
+    return this.requestHelper.post(
+              pathDocumentBase + '/' + applicationId + '/data/' + documentType,
+              'application/json',
+              { payload: payload }
+            );
+  }
 
 };
 
