4ad9e7bc1edd2f2bbd481fb74f90c508fc98c8ca
Lars Gregori <lars.gregori@sap.com>
2016-08-29 11:00:55 +0200

    echo test

--- test/test-yaas-echo.js
+++ test/test-yaas-echo.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 describe('Echo', function () {
 	describe('get token', function () {
 		it('should return data for a new created token', function (done) {
@@ -15,17 +14,28 @@ describe('Echo', function () {
 					var Echo = require('../yaas-echo.js');
 					var echo = new Echo();
 					echo.get(token)
-						.then(function (body) { 		
-							console.log('ECHO', body); 
-						
-							body.should.not.be.empty();
-
-							// TODO test
-							// hybris-client
-							// hybris-client-id
-					});
+						.then(function (res) {
+							res.should.not.be.empty();
+							res.should.have.property('statusCode', 200);
+							var body = res.body;
+							body.should.have.property('hybris-client', process.env.TEST_YAAS_IDENTIFIER);
+							body.should.have.property('hybris-client-id', process.env.TEST_YAAS_CLIENT_ID);
+							done();
+						});
 				});
-
 		})
+
+		it('should return an error for an unknown token', function (done) {
+
+			var Echo = require('../yaas-echo.js');
+			var echo = new Echo();
+			echo.get('unknown_token')
+				.then(function (res) {
+					res.should.not.be.empty();
+					res.should.have.property('statusCode', 401);
+					done();
+				});
+		})
+
 	})
 });
